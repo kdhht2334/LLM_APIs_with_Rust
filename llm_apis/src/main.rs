7a4ba6614786f06521e08113f840e72072b1045d
kdhht2334 <kdhht5022@gmail.com>
2024-07-19 03:03:05 +0900

    remove annotations

--- llm_apis/src/main.rs
+++ llm_apis/src/main.rs
@@ -16,23 +16,3 @@ async fn main() {
             .unwrap();
     }
 }
-
-// use kalosm::language::{prompt_input, TextStream};
-// use llm_apis::chat::loop_chat;
-
-// // run below ;)
-// // LIBCLANG_PATH=D:/clang+llvm-18.1.8-x86_64-pc-windows-msvc/clang+llvm-18.1.8-x86_64-pc-windows-msvc/lib cargo run --release
-
-// #[tokio::main]
-// async fn main() {
-//     let mut chat = loop_chat();
-
-//     loop {
-//         chat.add_message(prompt_input("\n> ").unwrap())
-//             .await
-//             .unwrap()
-//             .to_std_out()
-//             .await
-//             .unwrap();
-//     }
-// }
