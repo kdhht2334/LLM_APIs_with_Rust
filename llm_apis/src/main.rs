82f8a96340b040b7cebd6697964e28204a70a53b
kdhht2334 <kdhht5022@gmail.com>
2024-08-06 00:38:10 +0900

    feat: add tokenize function

--- llm_apis/src/main.rs
+++ llm_apis/src/main.rs
@@ -1,7 +1,7 @@
 use kalosm::language::*;
+use tokenizers::tokenizer::{Result, Tokenizer};
 
-#[tokio::main]
-async fn main() {
+async fn chat() {
     let mut model = Llama::new_chat();
     let mut chat = Chat::builder(&mut model)
         .with_system_prompt("The assistant help answer questions based on the context given by the user. The model knows that the information the user gives it is always true.")
@@ -14,5 +14,42 @@ async fn main() {
             .to_std_out()
             .await
             .unwrap();
+    }
+}
+
+fn token_encoding() -> Result<()> {
+    use std::io::{stdin, stdout, Write};
+
+    let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None)?;
+
+    loop {
+        let mut user_input = String::new();
+        print!("Please enter some text: ");
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut user_input)
+            .expect("Did not enter a correct string");
+
+        if user_input.is_empty() {
+            break;
+        }
+
+        let encoding = tokenizer.encode(user_input, false)?;
+        println!("{:?}", encoding.get_tokens());
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let action: &str = "token";
+
+    if action == "chat" {
+        chat().await;
+    } else if action == "token" {
+        token_encoding();
+    } else {
+        println!("{:?} is not valid action!", action);
     }
 }
