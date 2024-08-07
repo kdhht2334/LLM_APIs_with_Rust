cfb9b5de7ace1e653a9b1c457db86d165c38520a
kdhht2334 <kdhht5022@gmail.com>
2024-08-08 03:01:34 +0900

    feat: tokenizers (create and add vocab fns)

--- llm_apis/src/main.rs
+++ llm_apis/src/main.rs
@@ -1,5 +1,9 @@
 use kalosm::language::*;
-use tokenizers::tokenizer::{Result, Tokenizer};
+use tokenizers::models::bpe::{BpeTrainerBuilder, BPE};
+use tokenizers::normalizers::{strip::Strip, unicode::NFC, utils::Sequence};
+use tokenizers::pre_tokenizers::byte_level::ByteLevel;
+use tokenizers::tokenizer::Tokenizer;
+use tokenizers::{AddedToken, Result, TokenizerBuilder};
 
 async fn chat() {
     let mut model = Llama::new_chat();
@@ -41,14 +45,53 @@ fn token_encoding() -> Result<()> {
     Ok(())
 }
 
+fn train() -> Result<()> {
+    let vocab_size: usize = 100;
+    let text_file_fp: &str = "path/to/vocab.txt";
+    let save_path: &str = "path/to/tokenizer.json";
+
+    let mut trainer = BpeTrainerBuilder::new()
+        .show_progress(true)
+        .vocab_size(vocab_size)
+        .min_frequency(0)
+        .special_tokens(vec![
+            AddedToken::from(String::from("<s>"), true),
+            AddedToken::from(String::from("<pad>"), true),
+            AddedToken::from(String::from("</s>"), true),
+            AddedToken::from(String::from("<unk>"), true),
+            AddedToken::from(String::from("<mask>"), true),
+        ])
+        .build();
+
+    let mut tokenizer = TokenizerBuilder::new()
+        .with_model(BPE::default())
+        .with_normalizer(Some(Sequence::new(vec![
+            Strip::new(true, true).into(),
+            NFC.into(),
+        ])))
+        .with_pre_tokenizer(Some(ByteLevel::default()))
+        .with_post_processor(Some(ByteLevel::default()))
+        .with_decoder(Some(ByteLevel::default()))
+        .build()?;
+
+    let pretty = false;
+    tokenizer
+        .train_from_files(&mut trainer, vec![text_file_fp.to_string()])?
+        .save(save_path, pretty)?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
-    let action: &str = "token";
+    let action: &str = "train";
 
     if action == "chat" {
         chat().await;
-    } else if action == "token" {
-        token_encoding();
+    } else if action == "tokenize" {
+        let _ = token_encoding();
+    } else if action == "train" {
+        let _ = train();
     } else {
         println!("{:?} is not valid action!", action);
     }
